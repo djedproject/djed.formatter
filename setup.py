443c6c7250defd64d500723238d96c3ce3bae5f5
Marco Strobel <mail@strobel.ch>
2015-05-24 13:41:57 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -12,39 +12,38 @@ install_requires = [
     'babel',
     'pyramid',
     'pytz',
-    ]
+]
 
 tests_require = install_requires + [
     'nose',
     'webtest',
-    ]
+]
 
 
-setup(name='djed.formatter',
-      version='0.0',
-      description='djed.formatter',
-      long_description=README + '\n\n' + CHANGES,
-      classifiers=[
+setup(
+    name='djed.formatter',
+    version='0.1.dev0',
+    description='djed.formatter',
+    long_description='\n\n'.join([README, CHANGES]),
+    classifiers=[
         "Framework :: Pyramid",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: ISC License (ISCL)",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: Implementation :: CPython",
         "Topic :: Internet :: WWW/HTTP",
-        "Topic :: Internet :: WWW/HTTP :: WSGI",
-        ],
-      author='Djed developers',
-      author_email='djedproject@googlegroups.com',
-      url='https://github.com/djedproject/djed.formatter',
-      license='ISC License (ISCL)',
-      keywords='web pyramid pylons',
-      packages=find_packages(),
-      include_package_data=True,
-      zip_safe=False,
-      namespace_packages=['djed'],
-      install_requires=install_requires,
-      tests_require=tests_require,
-      test_suite='nose.collector',
-      )
+    ],
+    author='Djed developers',
+    author_email='djedproject@googlegroups.com',
+    url='https://github.com/djedproject/djed.formatter',
+    license='ISC License (ISCL)',
+    keywords='web pyramid pylons',
+    packages=['djed.formatter'],
+    include_package_data=True,
+    install_requires=install_requires,
+    extras_require={
+        'testing': tests_require,
+    },
+    test_suite='nose.collector',
+    )
